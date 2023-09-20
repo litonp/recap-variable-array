02fa70b11c3ebe8c54a5889b3b588293ad190323
litonp <litondas7121@gmail.com>
2023-09-20 13:15:53 +0600

    mobile

---
+++ conditionals.js
@@ -0,0 +1,11 @@
+/*
+CONDITIONALS
+1. Mening of : >, <, >=, <=, ==, !=, ===, !==, ?
+2.Mening of &&?
+3.Mening of ||?
+4.Let,s say you have x amount of money . if you have more than 80,000,then
+if more than 60,000 then you will by gaming laptop
+if you more than 40,000 then you will by lenovo Yoga
+if you have more than 20,000 then you will by an used laptop
+// otherwise, you will use you mobile phone?
+*/ 
