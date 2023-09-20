3098bb77f5cdd3d68c2493ba1aef7a36339b3725
litonp <litondas7121@gmail.com>
2023-09-20 13:05:54 +0600

    problem

---
+++ array.js
@@ -0,0 +1,14 @@
+/*
+ARRAY
+1. What is the purpose?
+2. How to declare an array in JS?
+3. Number of elementd in an Array?
+4. What is index?
+5. Find the value of an element by index?
+6. Change an element by index?
+7. Get the index of an element by the value?
+8. How can you add an element to an attay at the last position?
+9. How can you remove the last element from array?
+10. Add an element at the first position of an array?
+11. Remove the first element of an array?
+*/ 
