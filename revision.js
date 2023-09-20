9b22537a179d499766e1406b3430d57660dafd86
litonp <litondas7121@gmail.com>
2023-09-20 16:36:14 +0600

    revison

---
+++ revision.js
@@ -0,0 +1,85 @@
+//1. variable
+var Number = 10;
+
+//2. variable decleare
+var num1 = 10;
+var num2 = 20;
+totalNumber = num1 + num2;
+// console.log(totalNumber);
+
+//3. variable update
+var fruits = ['apple', 'orange', 'banana'];
+var bananaIndex = fruits.indexOf('banana');
+// console.log(bananaIndex);
+fruits[0] = 'mango';
+// console.log(fruits);
+fruits.pop();
+// console.log(fruits);
+fruits.push('watermalon');
+// console.log(fruits);
+
+//4. Type of variable
+// number type
+var number = 120;
+
+// string type
+var bottol = 'water';
+var bottolColor = 'yellow';
+
+// boolean type
+// var isExpensive = true;
+// var single = false;
+
+//5. variable name declear
+// var 45num
+// var num num
+// var num_num
+// var totalNumber
+
+// 6. Math operation
+// soman =
+// jog +
+// biyog -
+// gun *
+// bhag /
+// bhagshes %
+
+// 8.
+// ++ 1 jog kora 
+// -- 1 biyog kora
+
+// 9.
+// toFixed , decimal er por koto songkha bosbe
+// parent , 
+
+// 10
+var numbers = ['3', '6', '7', '9'];
+// console.log(numbers.length);
+var element = numbers[2];
+// console.log(element);
+var positionIndex = numbers.indexOf('9');
+// console.log(positionIndex);
+
+var money = 80000;
+if (money > 60000) {
+    console.log('phn');
+}
+
+var number = 581;
+for (var i = 581; i <= 623; i += 2) {
+    console.log(i);
+}
+var items = ['realme', 'saymphoney', 'itel', 'redmi'];
+for (var i = 0; i < items.length; i++) {
+    var item = items[i];
+    console.log(item);
+}
+
+var number = 30;
+for (var i = 30; i <= 86; i++) {
+    console.log(i);
+    if (i >= 44) {
+        break;
+    }
+}
+
